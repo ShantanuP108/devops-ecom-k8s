acfe0d28b411f5ad638a937404ca79a55d75e382
Shantanu Pandey <shantanup108@gmail.com>
2025-07-23 18:56:08 +0000

    feat: Update product service to V2 and add CD pipeline

--- services/product-service/src/main.py
+++ services/product-service/src/main.py
@@ -6,6 +6,6 @@ app = FastAPI(title="Product Service")
 @app.get("/")
 def read_root():
     """A simple root endpoint that returns a welcome message."""
-    return {"service": "Product Service", "status": "ok"}
+       return {"service": "Product Service V2", "status": "ok"}
 
 # Triggering a new 
